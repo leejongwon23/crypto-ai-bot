a286993ce1259be4f0f08c6a68306d15ad77447e
leejongwon23 <rnepfk@gmail.com>
2025-05-06 15:16:33 +0900

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,12 @@
+import os
 from telegram import Bot
 
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 def send_recommendation(msg):
+    if not BOT_TOKEN or not CHAT_ID:
+        print("❌ 텔레그램 환경변수 누락")
+        return
     bot = Bot(token=BOT_TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=msg)
