6ec05aa4202240645de833e40bf0adaa14be8465
leejongwon23 <rnepfk@gmail.com>
2025-05-10 22:15:32 +0900

    Update train.py

--- train.py
+++ train.py
@@ -103,10 +103,9 @@ def train_model(symbol, strategy, input_size=11, batch_size=32, epochs=10, lr=1e
     print("✅ models 폴더 생성됨", flush=True)
     print(f"✅ 모델 저장됨: {model_path}", flush=True)
 
-    with open("train_log.txt", "a") as f:
+    with open("train_log.txt", "a") as f:    
         
-    f.write
-    (f"[{datetime.datetime.utcnow()}] ✅ 저장됨: {model_path}\n")
+    f.write(f"[{datetime.datetime.utcnow()}] ✅ 저장됨: {model_path}\n")
 
 
     print("📁 models 폴더 내용:")
