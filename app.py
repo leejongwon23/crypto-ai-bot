123c28a802af687ee7e6f1a5ccd4989a17626806
leejongwon23 <rnepfk@gmail.com>
2025-06-29 08:10:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from wrong_data_loader import load_training_prediction_data
 from predict import evaluate_predictions
 from train import train_symbol_group_loop
 import maintenance_fix_meta
-maintenance_fix_meta.fix_all_meta_json()   # ✅ 이렇게 호출 추가
 
 PERSIST_DIR = "/persistent"
 LOG_DIR, MODEL_DIR = os.path.join(PERSIST_DIR, "logs"), os.path.join(PERSIST_DIR, "models")
