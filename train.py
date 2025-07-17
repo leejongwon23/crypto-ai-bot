c6e0b90bc35d81dc4672b6bdfb4c01377e0ef3a0
leejongwon23 <rnepfk@gmail.com>
2025-07-17 15:41:41 +0900

    Update train.py

--- train.py
+++ train.py
@@ -115,6 +115,7 @@ def train_one_model(symbol, strategy, group_id=None, max_epochs=20):
     from window_optimizer import find_best_windows
     from data.utils import get_kline_by_strategy, compute_features, create_dataset
     from focal_loss import FocalLoss
+    from config import get_class_groups
     from meta_learning import maml_train_entry
     import pytz
 
