ce23d09352d7c315a8ad45d28b68ad4f061ff5c3
leejongwon23 <rnepfk@gmail.com>
2025-09-13 06:32:48 +0900

    Update config.py

--- config.py
+++ config.py
@@ -556,4 +556,4 @@ __all__ = [
     "get_ORDERED_TRAIN", "get_PREDICT_MIN_RETURN", "get_DISPLAY_MIN_RETURN",
     "get_SSL_CACHE_DIR",
     "FEATURE_INPUT_SIZE", "NUM_CLASSES", "FAIL_AUGMENT_RATIO", "MIN_FEATURES",
-        ]
+    ]
